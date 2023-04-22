a9bed2270cb8e5323615fee6d66a914765bd81a1
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-04-22 16:03:21 +0530

    upd

--- 10Slices/main.go
+++ 10Slices/main.go
@@ -6,18 +6,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to video on slices")
+	// fmt.Println("Welcome to video on slices")
 
-	var fruitList =[]string{"Apple","Tomato","peach"}
-	fmt.Println("Value of fruit list",fruitList)
-	fmt.Printf("Type of fruitlist is %T \n", fruitList)
+	// var fruitList =[]string{"Apple","Tomato","peach"}
+	// fmt.Println("Value of fruit list",fruitList)
+	// fmt.Printf("Type of fruitlist is %T \n", fruitList)
 
-	fruitList =append(fruitList, "mango","banana")
-	fmt.Println("Value of fruit list is", fruitList)
+	// fruitList =append(fruitList, "mango","banana")
+	// fmt.Println("Value of fruit list is", fruitList)
 
-	//slices
-	fruitList =append(fruitList[1:3])   // [1:] remove the first element ,[1:3] print 1 amd 2 
-	fmt.Println(fruitList)
+	// //slices
+	// fruitList =append(fruitList[1:3])   // [1:] remove the first element ,[1:3] print 1 amd 2 
+	// fmt.Println(fruitList)
 
 	highScorse :=make([]int ,4)
 	highScorse[0] = 900	
@@ -28,4 +28,14 @@ func main() {
 	highScorse = append(highScorse, 444,456,567) // will append new values
 	sort.Ints(highScorse) // provide sort function and many more functions
 	fmt.Println(highScorse)
+
+	//how to remove a value from slice based on index
+	var courses =[]string{"react.js","node.js","java","javascript"}
+	fmt.Println(courses)
+	fmt.Println(len(courses))
+	var idx int = 2
+	courses =append(courses[:idx],courses[idx+1:]... )
+	fmt.Println(courses)
+	fmt.Println(len(courses))
+
 }
